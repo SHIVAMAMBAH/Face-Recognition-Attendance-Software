d5aa069cadaaaa92875ea5a9384ea6bd7d6af4ae
SDiHALF <shivamsharma22468@gmail.com>
2024-11-17 17:43:10 +0530

    Update image_insert.py

--- image_insert.py
+++ image_insert.py
@@ -1,1 +1,11 @@
+import face_recognition
+
+image_path = r''
+image = face_recognition.load_image_file(image_path)
+encodings = face_recognition.face_encodings(image)
+
+if len(encodings) == 0:
+    print("No face found in the image.")
+else:
+    print("Face encoding generated successfully.")
 
