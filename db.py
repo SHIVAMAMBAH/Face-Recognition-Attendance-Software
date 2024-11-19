86979c661b9201ea2a8a103c91eb2b4b188942b0
SDiHALF <shivamsharma22468@gmail.com>
2024-11-19 22:02:44 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,4 @@
 
-# #     return binary_data
-
 # # # Function to insert image into SQLite database
 # # def insert_image(name, roll_number, image_path):
 # #     image = convert_to_binary_data(image_path)
