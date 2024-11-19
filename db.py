25858ea1aaed39d158ec5a3bab37c7a2a3dce9cf
SDiHALF <shivamsharma22468@gmail.com>
2024-11-19 22:09:26 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,4 @@
 
-
-# # Update the table structure to store face encodings
 # cursor.execute('''CREATE TABLE IF NOT EXISTS students (
 #                     id INTEGER PRIMARY KEY AUTOINCREMENT,
 #                     name TEXT NOT NULL,
